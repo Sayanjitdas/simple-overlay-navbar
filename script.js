ed566c7929edb0a51249e5ce0bdb457620fed25b
sayanjit <sayanjitgit@gmail.com>
2020-08-29 22:07:24 +0530

    commit which added the whole navbar overlay..

--- script.js
+++ script.js
@@ -2,10 +2,17 @@
 let icon = document.getElementById('icon-frame');
 let lineOne = document.getElementById('line1');
 let lineThree = document.getElementById('line3');
+let navbarFrame = document.getElementById('navbar-overlay-frame');
+
 
 icon.addEventListener('click',function(){
+     navbarFrame.removeAttribute('style');
      lineOne.classList.toggle('line1-crossed');
      lineThree.classList.toggle('line3-crossed');
      lineOne.classList.toggle('line1');
      lineThree.classList.toggle('line3');
+     navbarFrame.classList.toggle('scale-out-tr');
+     navbarFrame.classList.toggle('scale-in-tr');
+   
 })
+
